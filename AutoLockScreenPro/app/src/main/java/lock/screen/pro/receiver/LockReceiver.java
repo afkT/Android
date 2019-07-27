5b2d3add299bab31fe91015bd552c6805720d373
Ttt <13798405957@163.com>
2019-07-28 06:50:04 +0800

    AutoLockScreenPro - 推送自动锁屏功能 项目

---
+++ AutoLockScreenPro/app/src/main/java/lock/screen/pro/receiver/LockReceiver.java
@@ -0,0 +1,10 @@
+package lock.screen.pro.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * detail: 锁屏广播组件
+ * Created by Ttt
+ */
+public class LockReceiver extends DeviceAdminReceiver {
+}
